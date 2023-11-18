f9632972f87cb3087850f489a023477b392c942f
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-11-18 23:35:54 +0530

    Create ValidPalindrome.py

---
+++ ValidPalindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = ""
+        for char in s:
+            if char.isalnum()  == True:
+                string += char.lower()
+        print(string)
+        return string == string[::-1]
