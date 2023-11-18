b82d156bca44e3b988d27af48167615097c8e956
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-11-18 23:43:06 +0530

    Update EncodeDecodeString.py

--- EncodeDecodeString.py
+++ EncodeDecodeString.py
@@ -1,5 +1,19 @@
 class solution:
   def encode(self, strs):
     result = ""
-    for char in strs:
-      pass
+    for word in strs:
+      result += str(len(word)) + '/' + word
+    return result
+
+  def decode(self, str):
+    result = list()
+    i = 0
+    for char in str:
+      e = i
+      while e < len(str) and e != '/':
+        e += 1
+      size = int(str[i:e])
+      word = str[e+1: e+1+size]
+      i = e + 1 + size
+      result.append(word)
+    return result
