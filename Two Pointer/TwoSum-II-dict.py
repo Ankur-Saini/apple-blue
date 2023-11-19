e30ea55f62e4486efe7746a6da2f5f1d0d555783
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-11-19 23:31:19 +0530

    Create TwoSum-II-dict.py

---
+++ Two Pointer/TwoSum-II-dict.py
@@ -0,0 +1,8 @@
+# Dictionary approach
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(numbers):
+            if target - num in num_dict:
+                return [num_dict[target - num] + 1, i + 1]
+            num_dict[num] = i
