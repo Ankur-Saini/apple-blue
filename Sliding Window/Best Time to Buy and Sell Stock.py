0087628a404ecdf7803db00731f4deb49ae0dc05
Ankur Saini <102854487+Ankur-Saini@users.noreply.github.com>
2023-11-21 23:36:00 +0530

    Create Best Time to Buy and Sell Stock.py

---
+++ Sliding Window/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        left = 0
+        right = 1
+        max_profit = profit = 0
+        while right < n:
+            if prices[right] < prices[left]:
+                left = right
+            else:
+                profit = prices[right] - prices[left]
+                if profit > max_profit:
+                    max_profit = profit
+            right += 1
+        return max_profit
